41509821b091768712e1021c533f0727cf82390a
Quntarious <90361495+Quntarious@users.noreply.github.com>
2022-05-22 22:36:51 -0500

    updating server

--- server.js
+++ server.js
@@ -1,30 +1,47 @@
-const express = require("express");
-const logger = require("morgan");
-const mongoose = require("mongoose");
-const compression = require("compression");
+const express = require('express');
+const path = require('path');
+//import apollo server
+const { ApolloServer } = require('apollo-server-express');
+// import typeDefs and resolvers
+const { typeDefs, resolvers} = require('./schemas');
+const {authMiddleware} = require('./utils/auth');
 
-const PORT = process.env.PORT || 3001;
-const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+//db connection
+const db = require('./config/connection');
 
+// const routes = require('./routes');
+
+//express server
 const app = express();
+const PORT = process.env.PORT || 3001;
 
-app.use(logger("dev"));
+//apollo server
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware
+});
 
-app.use(compression());
+//apply apollo server with express app
+server.applyMiddleware({ app });
+
+//middleware parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(express.static("public"));
+// if we're in production, serve client/build as static assets
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../client/build')));
+}
 
-mongoose.connect(MONGODB_URI, {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-  useUnifiedTopology: true,
+//get all
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-// routes
-app.use(require("./routes/api.js"));
-
-app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+db.once('open', () => {
+  app.listen(PORT, () => {
+    console.log(`API server running on port ${PORT}!`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  });
 });
--- server/server.js
+++ server/server.js
@@ -1,28 +1,31 @@
 const express = require('express');
-const { ApolloServer } = require('apollo-server-express');
-const { authMiddleware } = require('./utils/auth');
-
 const path = require('path');
+//import apollo server
+const { ApolloServer } = require('apollo-server-express');
+// import typeDefs and resolvers
+const { typeDefs, resolvers} = require('./schemas');
+const {authMiddleware} = require('./utils/auth');
+
+//db connection
 const db = require('./config/connection');
-const routes = require('./routes');
 
+// const routes = require('./routes');
 
-//import our typeDefs and resolvers
-const { typeDefs, resolvers } = require('./schemas');
-
+//express server
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-//create new Apollo server and pass in our schema data
+//apollo server
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware
-})
+});
 
-//integrate our Apollo server with Express application as middleware
-server.applyMiddleware({app});
+//apply apollo server with express app
+server.applyMiddleware({ app });
 
+//middleware parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -31,24 +34,16 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
+// app.use(routes);
+
+//get all
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
-
-
-app.use(routes); //comment this out in the end
-
-
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
-    // log where we can go to test our GQL API
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-
   });
-});
-
-process.on('uncaughtException', function(err) {
-  console.log('Caught exception: ' + err);
 });
