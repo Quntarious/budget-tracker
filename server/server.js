be8f6f6319a62b11a5174c442a33c69a5c39b6dc
Quntarious <90361495+Quntarious@users.noreply.github.com>
2022-05-22 22:05:07 -0500

    updated schema

--- server/server.js
+++ server/server.js
@@ -1,31 +1,28 @@
 const express = require('express');
-const path = require('path');
-//import apollo server
 const { ApolloServer } = require('apollo-server-express');
-// import typeDefs and resolvers
-const { typeDefs, resolvers} = require('./schemas');
-const {authMiddleware} = require('./utils/auth');
+const { authMiddleware } = require('./utils/auth');
 
-//db connection
+const path = require('path');
 const db = require('./config/connection');
+const routes = require('./routes');
 
-// const routes = require('./routes');
 
-//express server
+//import our typeDefs and resolvers
+const { typeDefs, resolvers } = require('./schemas');
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-//apollo server
+//create new Apollo server and pass in our schema data
 const server = new ApolloServer({
   typeDefs,
   resolvers,
   context: authMiddleware
-});
+})
 
-//apply apollo server with express app
-server.applyMiddleware({ app });
+//integrate our Apollo server with Express application as middleware
+server.applyMiddleware({app});
 
-//middleware parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -34,16 +31,24 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-// app.use(routes);
-
-//get all
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
 });
 
+
+
+app.use(routes); //comment this out in the end
+
+
 db.once('open', () => {
   app.listen(PORT, () => {
     console.log(`API server running on port ${PORT}!`);
+    // log where we can go to test our GQL API
     console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+
   });
+});
+
+process.on('uncaughtException', function(err) {
+  console.log('Caught exception: ' + err);
 });
